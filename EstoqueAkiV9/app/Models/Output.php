fada832f0156692f6e0229fb8133f7822c2c7c4d
Felipe Erik <felipeerik1524@gmail.com>
2023-11-06 16:20:54 -0300

    ola

---
+++ EstoqueAkiV9/app/Models/Output.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Output extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'quantidade',
+        'tipo',
+    ];
+}
