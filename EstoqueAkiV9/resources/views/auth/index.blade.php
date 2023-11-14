24dcaf0a7db5fa726fe2514517a7954b308907ac
Felipe Erik <felipeerik1524@gmail.com>
2023-11-14 09:55:22 -0300

    merge

--- EstoqueAkiV9/resources/views/auth/index.blade.php
+++ EstoqueAkiV9/resources/views/auth/index.blade.php
@@ -52,43 +52,6 @@
                                         <h3 style="color: #13293D" class="sidebar-brand-text mx-3">EstoqueAki</h3>
                                     </div>
 
-<<<<<<< HEAD
-                                    <form class="user">
-                                        <div class="form-group">
-                                            <!-- <input type="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address..."> -->
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <!-- <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label> -->
-                                            </div>
-                                            <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Senha">
-                                        </div>
-                                        </div>
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                            Entrar
-                                        </a>
-                                        <!-- <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a> -->
-                                    </form>
-                                    <!-- <hr> -->
-                                    <div class="text-center">
-                                        <!-- <a class="small" href="forgot-password.html">Forgot Password?</a> -->
-                                    </div>
-                                    <div class="text-center">
-                                        <!-- <a class="small" href="register.html">Create an Account!</a> -->
-                                    </div>
-=======
                                     @if(session('error'))
                                         <div class="alert alert-danger">
                                             {{ session('error') }}
@@ -105,7 +68,6 @@
                                     </form>
                                     <hr>
                                    
->>>>>>> main
                                 </div>
                             </div>
                         </div>
